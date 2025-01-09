6d812f3378192215ff91320a05cc48805b763899
Enki <cell.enohp.apps@gmail.com>
2025-01-09 13:17:18 +0100

    initial project setup

---
+++ Test-Uebung/TestUebung.Logic/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿/// SYSTEM
+global using System;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Collections.Generic;
+
+namespace TestUebung.Logic;
